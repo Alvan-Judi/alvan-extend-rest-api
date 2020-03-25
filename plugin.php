46a5d4cf26f0d4abcac739650107f429dfe3f732
Alvan-Judi <vandepittealexis@gmail.com>
2020-03-25 18:28:46 +0100

    Add plugin information and languages folder

--- plugin.php
+++ plugin.php
@@ -3,4 +3,11 @@
 /**
  * Plugin Name: Alvan Extend REST API 
  * Plugin URI: https://github.com/Alvan-Judi/alvan-extend-rest-api.git
+ * Description: Allows you to add more datas to the post endpoint of the REST API
+ * Version: 1.0.0
+ * Author: Alexis Vandepitte
+ * Licence: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: aera
+ * Domain Path: /languages
  */
